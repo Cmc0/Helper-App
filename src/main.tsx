504631900a95447106234469742178bd80a5def4
cmc6 <dimlt@vip.qq.com>
2022-07-21 10:33:32 +0800

    【v0.0.0】【update】【升级 vite 3】

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ import 'moment/dist/locale/zh-cn';
 
 moment.locale('zh-cn');
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <ConfigProvider locale={zhCN}>
             <App/>
